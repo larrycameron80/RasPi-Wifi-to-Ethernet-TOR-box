9a0669fbf0a8cd5a9f91af4467964df69664ea7d
Jonathan Evason <JEvason90@users.noreply.github.com>
2016-03-15 19:44:47 +0000

    Update torSwitch.py

--- torSwitch.py
+++ torSwitch.py
@@ -14,4 +14,13 @@ def main():
     main()
 
 def tor_on():
+ subprocess.call(['service', 'tor', 'restart'])
  subprocess.call(['./runtor'])
+ print('Alert! Network traffic now routed through TOR')
+
+
+def tor_off():
+ subprocess.call(['service', 'tor', 'stop'])
+ subprocess.call(['./resetiptables'])
+ subprocess.call(['./setupiptables'])
+ print('Alert! Network traffic no longer routed through TOR')
